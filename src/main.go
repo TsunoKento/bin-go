9cd6ae6fca5ff3973d702b7f97f193f839767e5e
TsunoKento <jinyejianren@gmail.com>
2023-01-02 23:47:38 +0900

    抽選される数字をまとめた配列の作成

--- src/main.go
+++ src/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	maxBingoNum   int = 50
+	bingoCardSize int = 25
+)
 
 func main() {
 	createBingoCard()
@@ -11,4 +20,21 @@ func main() {
 // 真ん中はすでに空いている状態
 func createBingoCard() {
 	fmt.Println("ビンゴカードを作成します。")
+
+	numList := createNumList()
+	var card [bingoCardSize]int
+	for i := 0; i < bingoCardSize; i++ {
+		rand.Seed(time.Now().UnixNano())
+		// 数字が重複する可能性があるビンゴカードが作成されている
+		card[i] = numList[rand.Intn(50)]
+	}
+}
+
+func createNumList() [maxBingoNum]int {
+	var numList [maxBingoNum]int
+	for i := 1; i <= maxBingoNum; i++ {
+		numList[i-1] = i
+	}
+
+	return numList
 }
