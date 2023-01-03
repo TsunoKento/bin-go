775504eb07aff41133d5db026916086d00b3238d
TsunoKento <jinyejianren@gmail.com>
2023-01-03 23:44:55 +0900

    ビンゴカードの真ん中は既に開けるように修正

--- src/main.go
+++ src/main.go
@@ -17,13 +17,16 @@ func main() {
 
 // 5*5で作成する
 // 数字は1~50の間で重複なしのランダム
-// 真ん中はすでに空いている状態
+// 真ん中はすでに空いている(0)状態
 func createBingoCard() {
 	fmt.Println("ビンゴカードを作成します。")
 
 	numList := createNumList()
 	var card [bingoCardSize]int
 	for i := 0; i < bingoCardSize; i++ {
+		if i == bingoCardSize/2 {
+			continue
+		}
 		var randomKey int
 		for {
 			rand.Seed(time.Now().UnixNano())
